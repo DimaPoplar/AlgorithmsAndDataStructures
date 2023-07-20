b5c3ebe23757625fc71f3b2bdd9ef5551e3f056b
DimaGrishin <amvlastik@gmail.com>
2023-07-20 12:39:10 +0300

    Дз 1

---
+++ src/main/java/HomeWork1/HeapSort.java
@@ -0,0 +1,49 @@
+package HomeWork1;
+
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+
+        heapSort(array);
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void heapSort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = rootIndex * 2 + 1;
+        int rightChild = rootIndex * 2 + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < heapSize && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, heapSize, largest);
+        }
+    }
+}
